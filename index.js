c2bcbeca39f9249ac5ac1dffe8912d3942d79642
tanjiro0708 <159068736+tanjiro0708@users.noreply.github.com>
2024-05-26 17:03:27 +0100

    Update index.js

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ require("./bot.js");
 ║                                                                        ║
 ║  ## Created by GlaceYT!                                                ║
 ║  ## Feel free to utilize any portion of the code                       ║
-║  ## DISCORD :  https://discord.com/invite/xQF9f9yUEM                   ║
-║  ## YouTube : https://www.youtube.com/@GlaceYt                         ║
+║  ## DISCORD :  https://discord.gg/jASszHTTXD                           ║
+║  ## YouTube : https://www.youtube.com/channel/UCopUn1EsL60mA65_bG3TT-Q ║
 ║                                                                        ║
 ╚════════════════════════════════════════════════════════════════════════╝
 
