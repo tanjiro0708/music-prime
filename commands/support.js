e25e81c4203c9050c8cca2282fec089cb0407955
tanjiro0708 <159068736+tanjiro0708@users.noreply.github.com>
2024-05-26 17:09:44 +0100

    Update support.js

--- commands/support.js
+++ commands/support.js
@@ -11,8 +11,8 @@
 ║                                                                        ║
 ║  ## Created by GlaceYT!                                                ║
 ║  ## Feel free to utilize any portion of the code                       ║
-║  ## DISCORD :  https://discord.com/invite/xQF9f9yUEM                   ║
-║  ## YouTube : https://www.youtube.com/@GlaceYt                         ║
+║  ## DISCORD :  https://discord.gg/jASszHTTXD                           ║
+║  ## YouTube : https://www.youtube.com/channel/UCopUn1EsL60mA65_bG3TT-Q ║
 ║                                                                        ║
 ╚════════════════════════════════════════════════════════════════════════╝
 
@@ -29,19 +29,19 @@ module.exports = {
   run: async (client, interaction) => {
     try {
 
-      const supportServerLink = "https://discord.gg/xQF9f9yUEM";
-      const githubLink = "https://github.com/GlaceYT";
-      const replitLink = "https://replit.com/@GlaceYT";
-      const youtubeLink = "https://www.youtube.com/@GlaceYT";
+      const supportServerLink = "https://discord.gg/jASszHTTXD";
+    //  const githubLink = "None";
+     // const replitLink = "None";
+      const youtubeLink = "https://www.youtube.com/channel/UCopUn1EsL60mA65_bG3TT-Q";
         const embed = new EmbedBuilder()
             .setColor('#b300ff')
             .setAuthor({
               name: 'Support Server',
-              iconURL: 'https://cdn.discordapp.com/attachments/1230824451990622299/1230824519220985896/6280-2.gif?ex=6638ae28&is=66375ca8&hm=13e4a1b91a95b2934a39de1876e66c11711c7b30ac1a91c2a158f2f2ed1c2fc6&', 
-              url: 'https://discord.gg/xQF9f9yUEM'
+              iconURL: 'https://media.discordapp.net/attachments/1223287062384607303/1242925221472632904/dawww.png?ex=66543911&is=6652e791&hm=0a98b309a036a4c8770c1875c046c24524e5591586bde61ea66058a7bee20c0f&=&format=webp&quality=lossless&width=840&height=473', 
+              url: 'https://discord.gg/jASszHTTXD'
           })
             .setDescription(`➡️ **Join our Discord server for support and updates:**\n- Discord - ${supportServerLink}\n\n➡️ **Follow us on:**\n- GitHub - ${githubLink}\n- Replit - ${replitLink}\n- YouTube - ${youtubeLink}`)
-            .setImage('https://cdn.discordapp.com/attachments/1113800537402527903/1236803979996958740/11.png?ex=663956f7&is=66380577&hm=3b3c19a11adcb979517a133f2907f671305d23f1f5092cf7df043e6d5cab07bc&')
+              .setImage('https://media.discordapp.net/attachments/1223287062384607303/1242925221472632904/dawww.png?ex=66543911&is=6652e791&hm=0a98b309a036a4c8770c1875c046c24524e5591586bde61ea66058a7bee20c0f&=&format=webp&quality=lossless&width=840&height=473')
             .setTimestamp();
 
       return interaction.reply({ embeds: [embed] });
@@ -64,8 +64,8 @@ module.exports = {
 ║                                                                        ║
 ║  ## Created by GlaceYT!                                                ║
 ║  ## Feel free to utilize any portion of the code                       ║
-║  ## DISCORD :  https://discord.com/invite/xQF9f9yUEM                   ║
-║  ## YouTube : https://www.youtube.com/@GlaceYt                         ║
+║  ## DISCORD :  https://discord.gg/jASszHTTXD                           ║
+║  ## YouTube : https://www.youtube.com/channel/UCopUn1EsL60mA65_bG3TT-Q ║
 ║                                                                        ║
 ╚════════════════════════════════════════════════════════════════════════╝
 
